4e6c0fc2aac165b40c09d4218b643c3ead019f70
Jake <jakedonbavand@gmail.com>
2020-01-19 21:00:21 +0000

    Initial player movement & anims
    Added test sprites, basic player movement scripts & anims.
    
    Currently bugged (moving left, player looks right) but functional.

--- Assets/Scripts/character_controller.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class character_controller : MonoBehaviour
-{
-    // This script controls all shared character functions and values, across player and NPCs
-}
